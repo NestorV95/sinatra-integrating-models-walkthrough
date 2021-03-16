bac4ff2e50dd6755860e810b8e1175772b527f98
Nestor <nestor.venegas95@gmail.com>
2021-03-16 18:03:30 -0500

    commit tree 5301f149e34c9d6c580805dbabea43d01a594222

--- app.rb
+++ app.rb
@@ -7,8 +7,13 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    @wc = @analyzed_text.count_of_words
+    @vc = @analyzed_text.count_of_vowels
+    @cc = @analyzed_text.count_of_consonants
+    @mul = @analyzed_text.most_used_letter.shift
+    
     erb :results
   end
+
 end
